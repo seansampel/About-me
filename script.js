c6263d1edd32dc99f1fab09f76fada8883e6233d
Sean <53915643+sampelmedia@users.noreply.github.com>
2019-10-07 22:09:47 +0100

    add test

---
+++ script.js
@@ -0,0 +1,13 @@
+
+        alert('Hello World');
+         
+           type="Bookings">
+
+           function confirm_email(){
+               var Bookings_name = document.getElementById('name');
+               var Bookings_email = document.getElementById('email');
+               var Bookings_message = document.getElementById('message');
+                   }
+                 
+                  
+                
